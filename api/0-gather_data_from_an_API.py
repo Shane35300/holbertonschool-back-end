1e66c6c3c2cd59eff7929d936944b732a06d2054
shane <6874@holbertonstudents.com>
2023-12-18 13:37:23 +0100

    task 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
 
-import urllib.request
-import sys
 import json
+import sys
+import urllib.request
 
+if __name__ == '__main__':
+    url_todos = "https://jsonplaceholder.typicode.com/todos"
+    url_users = "https://jsonplaceholder.typicode.com/users"
 
-url_todos = "https://jsonplaceholder.typicode.com/todos"
-url_users = "https://jsonplaceholder.typicode.com/users"
+    with urllib.request.urlopen(url_todos) as response:
+        data_todos = response.read()
+    with urllib.request.urlopen(url_users) as response:
+        data_users = response.read()
+    todos = json.loads(data_todos)
+    users = json.loads(data_users)
 
-
-with urllib.request.urlopen(url_todos) as response:
-    data_todos = response.read()
-with urllib.request.urlopen(url_users) as response:
-    data_users = response.read()
-todos = json.loads(data_todos)
-users = json.loads(data_users)
-
-for user in users:
-    if user['id'] == int(sys.argv[1]):
-        user_name = user['name']
-        tasks = 0
-        total_tasks = 0
-        for todo in todos:
-            if (todo['userId'] == int(sys.argv[1]) and
-                                  todo['completed'] == True):
-                tasks += 1
-            if todo['userId'] == int(sys.argv[1]):
-                total_tasks += 1
-print("Employee {} is done with tasks({}/{}):".format(user_name, tasks, total_tasks))
-for todo in todos:
-    if (todo['userId'] == int(sys.argv[1]) and todo['completed'] == True):
-        print(f"\t {todo['title']}")
+    for user in users:
+        if user['id'] == int(sys.argv[1]):
+            user_name = user['name']
+            tasks = 0
+            total_tasks = 0
+            for todo in todos:
+                if (todo['userId'] == int(sys.argv[1]) and
+                        todo['completed'] == True):
+                    tasks += 1
+                if todo['userId'] == int(sys.argv[1]):
+                    total_tasks += 1
+    print("Employee {} is done with tasks({}/{}):".format(user_name,
+                                                          tasks, total_tasks))
+    for todo in todos:
+        if (todo['userId'] == int(sys.argv[1]) and todo['completed'] == True):
+            print(f"\t {todo['title']}")
---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import csv
+import json
+import sys
+import urllib.request
+
+url_todos = "https://jsonplaceholder.typicode.com/todos"
+url_users = "https://jsonplaceholder.typicode.com/users"
+
+
+with urllib.request.urlopen(url_todos) as response:
+    data_todos = response.read()
+with urllib.request.urlopen(url_users) as response:
+    data_users = response.read()
+todos = json.loads(data_todos)
+users = json.loads(data_users)
+
+for user in users:
+    if user['id'] == int(sys.argv[1]):
+        username = user['username']
+todo_list = []
+for todo in todos:
+    dico = {}
+    if todo['userId'] == int(sys.argv[1]):
+        dico['id'] = str(sys.argv[1])
+        dico['username'] = username
+        dico['completed'] = str(todo['completed'])
+        dico['title'] = todo['title']
+        todo_list.append(dico)
+# Définir les noms de colonnes pour le fichier CSV
+fields = ['id', 'username', 'completed', 'title']
+# Nom du fichier CSV
+csv_filename = f'{sys.argv[1]}.csv'
+# Écrire les données dans le fichier CSV
+with open(csv_filename, mode='w', newline='') as csv_file:
+    writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    # Écrire les lignes de données
+    writer.writerow(fields)
+    for todo in todo_list:
+        writer.writerow([todo[field] for field in fields])
