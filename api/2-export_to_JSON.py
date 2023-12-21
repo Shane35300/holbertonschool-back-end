779bb6dbca2b3ff23cf77a067d4818f7389faf05
shane <6874@holbertonstudents.com>
2023-12-21 12:17:14 +0100

    task 2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -6,33 +6,35 @@ import json
 import sys
 import urllib.request
 
-url_todos = "https://jsonplaceholder.typicode.com/todos"
-url_users = "https://jsonplaceholder.typicode.com/users"
+
+if __name__ == '__main__':
+    url_todos = "https://jsonplaceholder.typicode.com/todos"
+    url_users = "https://jsonplaceholder.typicode.com/users"
 
 
-with urllib.request.urlopen(url_todos) as response:
-    data_todos = response.read()
-with urllib.request.urlopen(url_users) as response:
-    data_users = response.read()
-todos = json.loads(data_todos)
-users = json.loads(data_users)
+    with urllib.request.urlopen(url_todos) as response:
+        data_todos = response.read()
+    with urllib.request.urlopen(url_users) as response:
+        data_users = response.read()
+    todos = json.loads(data_todos)
+    users = json.loads(data_users)
 
-for user in users:
-    if user['id'] == int(sys.argv[1]):
-        username = user['username']
-todo_list = []
-for todo in todos:
-    dico = {}
-    if todo['userId'] == int(sys.argv[1]):
-        dico['task'] = todo['title']
-        dico['completed'] = str(todo['completed'])
-        dico['username'] = username
-        todo_list.append(dico)
-dictionary = {}
-dictionary[str(sys.argv[1])] = todo_list
+    for user in users:
+        if user['id'] == int(sys.argv[1]):
+            username = user['username']
+    todo_list = []
+    for todo in todos:
+        dico = {}
+        if todo['userId'] == int(sys.argv[1]):
+            dico['task'] = todo['title']
+            dico['completed'] = str(todo['completed'])
+            dico['username'] = username
+            todo_list.append(dico)
+    dictionary = {}
+    dictionary[str(sys.argv[1])] = todo_list
 
-# Nom du fichier json
-json_filename = f'{sys.argv[1]}.json'
-# Écrire les données dans le fichier CSV
-with open(json_filename, mode='w') as json_file:
-    json.dump(dictionary, json_file)
+    # Nom du fichier json
+    json_filename = f'{sys.argv[1]}.json'
+    # Écrire les données dans le fichier CSV
+    with open(json_filename, mode='w') as json_file:
+        json.dump(dictionary, json_file)
