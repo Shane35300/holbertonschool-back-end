68ddd91bd40624430ca40f737416c0c1c22a9162
shane <6874@holbertonstudents.com>
2023-12-21 14:34:21 +0100

    task 3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
+"""Module that import data into JSON format
+"""
 
 import json
 import sys
 import urllib.request
 
-url_todos = "https://jsonplaceholder.typicode.com/todos"
-url_users = "https://jsonplaceholder.typicode.com/users"
 
+if __name__ == '__main__':
+    url_todos = "https://jsonplaceholder.typicode.com/todos"
+    url_users = "https://jsonplaceholder.typicode.com/users"
 
-with urllib.request.urlopen(url_todos) as response:
-    data_todos = response.read()
-with urllib.request.urlopen(url_users) as response:
-    data_users = response.read()
-todos = json.loads(data_todos)
-users = json.loads(data_users)
+    with urllib.request.urlopen(url_todos) as response:
+        data_todos = response.read()
+    with urllib.request.urlopen(url_users) as response:
+        data_users = response.read()
+    todos = json.loads(data_todos)
+    users = json.loads(data_users)
 
-users_dico = {}
-users_list = []
-for user in users:
-    if user['id'] not in users_list:
-        users_list.append(user['id'])
-        username = user['username']
-        todo_list = []
-        for todo in todos:
-            if todo['userId'] == user['id']:
-                dico = {}
-                dico['username'] = username
-                dico['task'] = todo['title']
-                dico['completed'] = str(todo['completed'])
-                todo_list.append(dico)
-    users_dico[user['id']] = todo_list
+    users_dico = {}
+    users_list = []
+    for user in users:
+        if user['id'] not in users_list:
+            users_list.append(user['id'])
+            username = user['username']
+            todo_list = []
+            for todo in todos:
+                if todo['userId'] == user['id']:
+                    dico = {}
+                    dico['username'] = username
+                    dico['task'] = todo['title']
+                    if str(todo['completed']) == 'True':
+                        dico['completed'] = True
+                    else:
+                        dico['completed'] = False
+                    todo_list.append(dico)
+        users_dico[user['id']] = todo_list
 
-# Écrire les données dans le fichier CSV
-with open('todo_all_employees.json', mode='w') as json_file:
-    json.dump(users_dico, json_file)
+    # Écrire les données dans le fichier CSV
+    with open('todo_all_employees.json', mode='w') as json_file:
+        json.dump(users_dico, json_file)
